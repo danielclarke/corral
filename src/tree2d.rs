815bb7c9330fc4e54010b7e14503f16734aac57d
daniel <danielclarke@wearepopgun.com>
2022-09-19 17:30:13 +1000

    partition with tests and insert draft

--- src/tree2d.rs
+++ src/tree2d.rs
@@ -1,5 +1,7 @@
-#[derive(Debug, PartialEq)]
-struct BoundingBox {
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct BoundingBox {
     x: u32,
     y: u32,
     width: u32,
@@ -7,8 +9,16 @@ struct BoundingBox {
 }
 
 impl BoundingBox {
-    fn area(&self) -> u32 {
+    fn size(&self) -> u32 {
         self.width * self.height
+    }
+
+    fn can_contain(&self, width: u32, height: u32) -> bool {
+        width < self.width && height < self.height
+    }
+
+    fn same_shape(&self, width: u32, height: u32) -> bool {
+        width == self.width && height == self.height
     }
 }
 
@@ -32,71 +42,149 @@ pub enum Tree2d<T> {
         bb: BoundingBox,
         right: Box<Self>,
         down: Box<Self>,
-        data: T,
+        data: Rc<T>,
     },
 }
 
 impl<T> Tree2d<T> {
-    fn new(data: T, width: u32, height: u32) -> Self {
+    // fn new(data: Rc<T>, width: u32, height: u32) -> Self {
+    //     Tree2d::Node {
+    //         bb: BoundingBox {
+    //             x: 0,
+    //             y: 0,
+    //             width,
+    //             height,
+    //         },
+    //         right: Box::new(Self::Leaf {
+    //             bb: BoundingBox {
+    //                 x: width,
+    //                 y: 0,
+    //                 width: 0,
+    //                 height: 0,
+    //             },
+    //         }),
+    //         down: Box::new(Self::Leaf {
+    //             bb: BoundingBox {
+    //                 x: 0,
+    //                 y: height,
+    //                 width: 0,
+    //                 height: 0,
+    //             },
+    //         }),
+    //         data,
+    //     }
+    // }
+
+    fn partition(data: Rc<T>, bb: BoundingBox, width: u32, height: u32) -> Self {
+        let width_remainder = bb.width - width;
+        let height_remainder = bb.height - height;
+
+        let (bb_right, bb_down) = if height_remainder > width_remainder {
+            // ---------------
+            // |  data  |    |
+            // ---------------
+            // |             |
+            // |             |
+            // ---------------
+            (
+                BoundingBox {
+                    x: bb.x + width,
+                    y: bb.y,
+                    width: width_remainder,
+                    height,
+                },
+                BoundingBox {
+                    x: bb.x,
+                    y: bb.y + height,
+                    width: bb.width,
+                    height: height_remainder,
+                },
+            )
+        } else {
+            // ---------------
+            // |     |       |
+            // |data |       |
+            // |     |       |
+            // ------|       |
+            // |     |       |
+            // |     |       |
+            // ---------------
+            (
+                BoundingBox {
+                    x: bb.x + width,
+                    y: bb.y,
+                    width: width_remainder,
+                    height: bb.height,
+                },
+                BoundingBox {
+                    x: bb.x,
+                    y: bb.y + height,
+                    width,
+                    height: height_remainder,
+                },
+            )
+        };
+
         Tree2d::Node {
-            bb: BoundingBox {
-                x: 0,
-                y: 0,
-                width,
-                height,
-            },
-            right: Box::new(Self::Leaf {
-                bb: BoundingBox {
-                    x: width,
-                    y: 0,
-                    width: 0,
-                    height: 0,
-                },
-            }),
-            down: Box::new(Self::Leaf {
-                bb: BoundingBox {
-                    x: 0,
-                    y: height,
-                    width: 0,
-                    height: 0,
-                },
-            }),
+            bb,
+            right: Box::new(Self::Leaf { bb: bb_right }),
+            down: Box::new(Self::Leaf { bb: bb_down }),
             data,
         }
     }
 
-    fn min_bounding_box(&self, width: u32, height: u32) -> (u32, u32) {
-        (0, 0)
+    fn insert(&mut self, data: Rc<T>, width: u32, height: u32) -> bool {
+        match self {
+            Self::Leaf { .. } => false,
+            Self::Node {
+                bb,
+                right,
+                down,
+                data: _,
+            } => {
+                if bb.can_contain(width, height) {
+                    match (&**right, &**down) {
+                        (Self::Leaf { bb: bb_right }, Self::Leaf { bb: bb_down }) => {
+                            if bb_right.can_contain(width, height) {
+                                *right = Box::new(Self::partition(data, *bb_right, width, height));
+                                true
+                            } else if bb_down.can_contain(width, height) {
+                                *down = Box::new(Self::partition(data, *bb_down, width, height));
+                                true
+                            } else {
+                                false
+                            }
+                        }
+                        (Self::Leaf { bb: bb_right }, Self::Node { .. }) => {
+                            if bb_right.can_contain(width, height) {
+                                *right = Box::new(Self::partition(data, *bb_right, width, height));
+                                true
+                            } else {
+                                down.insert(data, width, height)
+                            }
+                        }
+                        (Self::Node { .. }, Self::Leaf { bb: bb_down }) => {
+                            if bb_down.can_contain(width, height) {
+                                *down = Box::new(Self::partition(data, *bb_down, width, height));
+                                true
+                            } else {
+                                right.insert(data, width, height)
+                            }
+                        }
+                        (Self::Node { .. }, Self::Node { .. }) => {
+                            if right.insert(Rc::clone(&data), width, height) {
+                                true
+                            } else {
+                                down.insert(Rc::clone(&data), width, height)
+                            }
+                        }
+                    }
+                } else {
+                    false
+                }
+            }
+        }
     }
-
-    // fn insert(&mut self, data: T, width: u32, height: u32) -> Box<Self> {
-    //     match self {
-    //         Self::Leaf { x, y } => Box::new(Self::Node {
-    //             x: *x,
-    //             y: *y,
-    //             width,
-    //             height,
-    //             right: Box::new(Self::Leaf {
-    //                 x: *x + width,
-    //                 y: *y,
-    //             }),
-    //             down: Box::new(Self::Leaf {
-    //                 x: *x,
-    //                 y: *y + height,
-    //             }),
-    //             data,
-    //         }),
-    //         Self::Node {
-    //             x: _x,
-    //             y: _y,
-    //             width: _width,
-    //             height: _height,
-    //             right,
-    //             down: _down,
-    //             data: _data,
-    //         } => right.insert(data, width, height),
-    //     }
-    // }
 }
 
 #[cfg(test)]
@@ -105,64 +193,231 @@ mod tests {
 
     #[test]
     fn bb_horizontal_add() {
-        let bb1 = BoundingBox{x: 0, y: 0, width: 1, height: 1};
-        let bb2 = BoundingBox{x: 1, y: 0, width: 1, height: 1};
-        let expected_output = BoundingBox{x: 0, y: 0, width: 2, height: 1};
+        let bb1 = BoundingBox {
+            x: 0,
+            y: 0,
+            width: 1,
+            height: 1,
+        };
+        let bb2 = BoundingBox {
+            x: 1,
+            y: 0,
+            width: 1,
+            height: 1,
+        };
+        let expected_output = BoundingBox {
+            x: 0,
+            y: 0,
+            width: 2,
+            height: 1,
+        };
         assert_eq!(expected_output, &bb1 + &bb2);
         assert_eq!(expected_output, &bb2 + &bb1);
     }
 
     #[test]
     fn bb_horizontal_overlap_add() {
-        let bb1 = BoundingBox{x: 0, y: 0, width: 2, height: 1};
-        let bb2 = BoundingBox{x: 1, y: 0, width: 2, height: 1};
-        let expected_output = BoundingBox{x: 0, y: 0, width: 3, height: 1};
+        let bb1 = BoundingBox {
+            x: 0,
+            y: 0,
+            width: 2,
+            height: 1,
+        };
+        let bb2 = BoundingBox {
+            x: 1,
+            y: 0,
+            width: 2,
+            height: 1,
+        };
+        let expected_output = BoundingBox {
+            x: 0,
+            y: 0,
+            width: 3,
+            height: 1,
+        };
         assert_eq!(expected_output, &bb1 + &bb2);
         assert_eq!(expected_output, &bb2 + &bb1);
     }
 
     #[test]
     fn bb_vertival_add() {
-        let bb1 = BoundingBox{x: 0, y: 0, width: 1, height: 1};
-        let bb2 = BoundingBox{x: 0, y: 1, width: 1, height: 1};
-        let expected_output = BoundingBox{x: 0, y: 0, width: 1, height: 2};
+        let bb1 = BoundingBox {
+            x: 0,
+            y: 0,
+            width: 1,
+            height: 1,
+        };
+        let bb2 = BoundingBox {
+            x: 0,
+            y: 1,
+            width: 1,
+            height: 1,
+        };
+        let expected_output = BoundingBox {
+            x: 0,
+            y: 0,
+            width: 1,
+            height: 2,
+        };
         assert_eq!(expected_output, &bb1 + &bb2);
         assert_eq!(expected_output, &bb2 + &bb1);
     }
 
     #[test]
     fn bb_vertival_overlap_add() {
-        let bb1 = BoundingBox{x: 0, y: 0, width: 1, height: 2};
-        let bb2 = BoundingBox{x: 0, y: 1, width: 1, height: 2};
-        let expected_output = BoundingBox{x: 0, y: 0, width: 1, height: 3};
+        let bb1 = BoundingBox {
+            x: 0,
+            y: 0,
+            width: 1,
+            height: 2,
+        };
+        let bb2 = BoundingBox {
+            x: 0,
+            y: 1,
+            width: 1,
+            height: 2,
+        };
+        let expected_output = BoundingBox {
+            x: 0,
+            y: 0,
+            width: 1,
+            height: 3,
+        };
         assert_eq!(expected_output, &bb1 + &bb2);
         assert_eq!(expected_output, &bb2 + &bb1);
     }
 
     #[test]
     fn bb_diagonal_add() {
-        let bb1 = BoundingBox{x: 0, y: 0, width: 1, height: 1};
-        let bb2 = BoundingBox{x: 1, y: 1, width: 1, height: 1};
-        let expected_output = BoundingBox{x: 0, y: 0, width: 2, height: 2};
+        let bb1 = BoundingBox {
+            x: 0,
+            y: 0,
+            width: 1,
+            height: 1,
+        };
+        let bb2 = BoundingBox {
+            x: 1,
+            y: 1,
+            width: 1,
+            height: 1,
+        };
+        let expected_output = BoundingBox {
+            x: 0,
+            y: 0,
+            width: 2,
+            height: 2,
+        };
         assert_eq!(expected_output, &bb1 + &bb2);
         assert_eq!(expected_output, &bb2 + &bb1);
     }
 
     #[test]
     fn bb_diagonal_overlap_add() {
-        let bb1 = BoundingBox{x: 0, y: 0, width: 2, height: 2};
-        let bb2 = BoundingBox{x: 1, y: 1, width: 2, height: 2};
-        let expected_output = BoundingBox{x: 0, y: 0, width: 3, height: 3};
+        let bb1 = BoundingBox {
+            x: 0,
+            y: 0,
+            width: 2,
+            height: 2,
+        };
+        let bb2 = BoundingBox {
+            x: 1,
+            y: 1,
+            width: 2,
+            height: 2,
+        };
+        let expected_output = BoundingBox {
+            x: 0,
+            y: 0,
+            width: 3,
+            height: 3,
+        };
         assert_eq!(expected_output, &bb1 + &bb2);
         assert_eq!(expected_output, &bb2 + &bb1);
     }
 
     #[test]
     fn bb_diagonal_disjoint_add() {
-        let bb1 = BoundingBox{x: 0, y: 0, width: 1, height: 1};
-        let bb2 = BoundingBox{x: 2, y: 2, width: 1, height: 1};
-        let expected_output = BoundingBox{x: 0, y: 0, width: 3, height: 3};
+        let bb1 = BoundingBox {
+            x: 0,
+            y: 0,
+            width: 1,
+            height: 1,
+        };
+        let bb2 = BoundingBox {
+            x: 2,
+            y: 2,
+            width: 1,
+            height: 1,
+        };
+        let expected_output = BoundingBox {
+            x: 0,
+            y: 0,
+            width: 3,
+            height: 3,
+        };
         assert_eq!(expected_output, &bb1 + &bb2);
         assert_eq!(expected_output, &bb2 + &bb1);
+    }
+}
+
+#[cfg(test)]
+mod tree_2d_tests {
+    use super::*;
+
+    #[test]
+    fn new_tree() {
+        let data = Rc::new(1u32);
+        let width = 2u32;
+        let height = 2u32;
+
+        let tree = Tree2d::partition(
+            Rc::clone(&data),
+            BoundingBox {
+                x: 0,
+                y: 0,
+                width: 4,
+                height: 4,
+            },
+            width,
+            height,
+        );
+
+        let expected_bb_right = BoundingBox {
+            x: 2,
+            y: 0,
+            width: 2,
+            height: 4,
+        };
+
+        let expected_bb_down = BoundingBox {
+            x: 0,
+            y: 2,
+            width: 2,
+            height: 2,
+        };
+
+        match tree {
+            Tree2d::Leaf { .. } => {
+                assert!(false, "root should be a node")
+            }
+            Tree2d::Node { right, down, .. } => {
+                match *right {
+                    Tree2d::Leaf { bb } => {
+                        assert_eq!(expected_bb_right, bb);
+                    }
+                    Tree2d::Node { .. } => {
+                        assert!(false, "right remainder should be a leaf")
+                    }
+                };
+                match *down {
+                    Tree2d::Leaf { bb } => {
+                        assert_eq!(expected_bb_down, bb);
+                    }
+                    Tree2d::Node { .. } => {
+                        assert!(false, "down remainder should be a leaf")
+                    }
+                };
+            }
+        }
     }
 }
